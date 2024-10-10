dc9fc19c1069e245fac7e2befd56490a7607504c
Markus Leion <markus.leion@gmail.com>
2024-10-10 12:03:18 +0200

    Changed the names of the diffirent pages and paths

---
+++ BlazorAssignment/Components/Pages/NewUser.razor.cs
@@ -0,0 +1,12 @@
+namespace BlazorAssignment.Components.Pages
+{
+    public partial class NewUser
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
