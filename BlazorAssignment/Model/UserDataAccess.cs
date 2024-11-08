18eca1d1f1852d2c12de6375c8b0b261dacb9ef0
Markus Leion <markus.leion@gmail.com>
2024-11-08 09:14:38 +0100

    Fixed soUserName is visible

--- BlazorAssignment/Model/UserDataAccess.cs
+++ BlazorAssignment/Model/UserDataAccess.cs
@@ -40,7 +40,7 @@ namespace BlazorAssignment.Model
             {
                 new UserData(2, "Max Verstappen", "IamNumber1", "max.verstappen@gmail.com", "1763078985", new Address("Vasagatan", "Stockholm", "11522"), new Company("Google", "Dont be Evil")),
                 new UserData(1, "Lewis Hamilton", "IamSpeed", "lewis.hamilton@gmail.com", "5569132080",new Address("Kungsgatan", "Uppsala", "11140"), new Company("Apple", "Think Diffirent")),
-                new UserData(8, "Markus Pärson", "BlockMan", "markus.person@gmail.com", "7636758362", new Address("Stationsgatan", "Malmö", "21143"), new Company("Mojang", "Endless possibilities")),
+                new UserData(8, "Markus Pärson", "Notch", "markus.person@gmail.com", "7636758362", new Address("Stationsgatan", "Malmö", "21143"), new Company("Mojang", "Endless possibilities")),
                 new UserData(4, "Zara Larsson", "Zara", "zara.larsson@gmail.com", "9608411182", new Address("Stefansgatan", "Stockholm", "70374"), new Company("Microsoft", "Embrace, extend, and extinguish")),
                 new UserData(3, "Sanna Marin", "FinnishLeader", "sanna.marin@gmail.com", "7053629083", new Address("Markgatan", "Borås", "74636"), new Company("Nvidia", "Graphics Reinvented")),
                 new UserData(10, "Johan Lundgren", "GhostMariner", "johan.lundgren@gmail.com", "7546463303", new Address("Apelvägen", "Uppsala", "17930"), new Company("Amazon", "From A to Z")),
